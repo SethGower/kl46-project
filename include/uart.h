ee0538d0bfe1b7f1aba7acf9d5b110b96307d70d
Seth Gower <sdg7234@rit.edu>
2020-02-09 19:31:31 -0500

    revamps uart defines

--- include/uart.h
+++ include/uart.h
@@ -3,7 +3,7 @@
 #include "MKL46Z4.h"
 #include "queue.h"
 #include <errno.h>
-void UART0_ISR(void);
+void UART0_IRQHandler(void);
 void Init_UART0_IRQ(void);
 void putChar(char c);
 char getChar(void);
@@ -25,11 +25,15 @@ char getChar(void);
 #define UART0_C2_T_RI (UART0_C2_RIE_MASK | UART0_C2_T_R)
 #define UART0_C2_TI_RI (UART0_C2_TIE_MASK | UART0_C2_T_RI)
 #define NVIC_IPR_UART0_MASK (0xC0)
-#define NVIC_ICPR_UART0_MASK (1u << 28)
-#define UART0_BDL_9600 0x80
-#define UART0_BDH_9600 0x25
-#define UART0_8N1 0x0
+#define UART0_IRQ_MASK (1u << 12)
+#define NVIC_ICPR_UART0_MASK UART0_IRQ_MASK
+#define NVIC_ISER_UART0_MASK UART0_IRQ_MASK
+#define UART0_BDH_9600 (0x01u)
+#define UART0_BDL_9600 (0x38u)
+#define UART0_8N1 0x00
 #define UART0_C3_TXINV 0x10
-#define UART0_C4_NO_MATCH_OSR_16 0x00
+#define UART0_C4_NO_MATCH_OSR_16 0x0Fu
 #define UART0_C5_NO_DMA_SSR_SYNC 0x00
+#define UART0_S1_CLEAR_FLAGS 0x1Fu
+#define UART0_S2_NO_RXINV_BRK10_NO_LBKDETECT_CLEAR_FLAGS 0xC0
 #endif
